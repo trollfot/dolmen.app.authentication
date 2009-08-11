32cbe133522ff6adfb8ec9c49715d1cf2c29adf1
Souheil CHELFOUH <trollfot@vittra.org>
2009-08-11 16:16:05 +0200

    Corrected login form to work with z3c.form.

--- src/dolmen/app/authentication/browser/login.py
+++ src/dolmen/app/authentication/browser/login.py
@@ -5,6 +5,8 @@ import grok
 import megrok.menu
 import zope.schema
 import zope.interface
+import megrok.z3cform
+
 from zope.event import notify
 from zope.component import getUtility
 from zope.i18nmessageid import MessageFactory
@@ -38,13 +40,19 @@ class Login(Form):
     grok.require('zope.Public')
     grok.context(zope.interface.Interface)
     megrok.menu.menuitem(UserAnonymousMenu)
-    
-    prefix = ""
-    form_name = _(u"Login form")
-    form_fields = grok.AutoFields(ILoginForm)
 
-    @grok.action(_(u"Log in"))
-    def login(self, login, password):
+    prefix = ""
+    label = _(u"Identify yourself")
+    form_name = _(u"Login form")
+    fields = megrok.z3cform.field.Fields(ILoginForm)
+
+    def updateWidgets(self):
+        Form.updateWidgets(self)
+        self.widgets.prefix = ''
+        self.widgets.update()
+
+    @megrok.z3cform.button.buttonAndHandler(_('Log in'), name='login')
+    def login(self, data):
         if IUnauthenticatedPrincipal.providedBy(self.request.principal):
             self.status = _(u"Login failed")
         else:
