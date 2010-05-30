2bf2283f5b1503e145ecdf5d3843c88247f5d80f
Souheil CHELFOUH <trollfot@gmail.com>
2010-05-30 16:41:11 +0200

    Used full path imports

--- src/dolmen/app/authentication/browser/__init__.py
+++ src/dolmen/app/authentication/browser/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from actions import AnonymousActionsMenu, AnonymousMenuEntry
-from actions import UserActionsMenu, UserMenuEntry
-from login import ILoginForm, Login
-from logout import LogoutAction, LoggedOut
+from dolmen.app.authentication.browser.actions import (
+    AnonymousActionsMenu, AnonymousMenuEntry, UserActionsMenu, UserMenuEntry)
+
+from dolmen.app.authentication.browser.login import ILoginForm, Login
+from dolmen.app.authentication.browser.logout import LogoutAction, LoggedOut
