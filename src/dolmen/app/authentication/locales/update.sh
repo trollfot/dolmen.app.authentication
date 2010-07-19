9d9bc6f9c8e62ab2948019b5e7ec2aa1d95ba071
tdesvenain <thomas.desvenain@gmail.com>
2010-07-19 16:16:12 +0200

    some internationalization and french translation

---
+++ src/dolmen/app/authentication/locales/update.sh
@@ -0,0 +1,2 @@
+i18ndude rebuild-pot --pot dolmen.app.authentication.pot --create dolmen.app.authentication ../
+i18ndude sync --pot dolmen.app.authentication.pot ./*/LC_MESSAGES/dolmen.app.authentication.po
