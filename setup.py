a13352976fa34a5b0cd0d0741744f3945860e44a
Souheil CHELFOUH <trollfot@gmail.com>
2010-07-28 22:39:57 +0200

    Ready for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join
 
 name = 'dolmen.app.authentication'
-version = '1.0a4dev'
+version = '1.0b1'
 readme = open(
     join('src', 'dolmen', 'app', 'authentication', 'README.txt')).read()
 history = open(
